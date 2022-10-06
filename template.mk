f2946835ac87f4f0120ad216f9110803c5291e29
joachim <jgiron@student.42.fr>
2022-10-06 15:33:08 +0200

    first commit

---
+++ template.mk
@@ -0,0 +1,48 @@
+SRCS_DIR := src
+
+OBJS_DIR := obj
+
+INCLUDE_DIR := includes
+
+OBJS_DIR_TREE := $(sort $(addprefix ${OBJS_DIR}/, $(dir ${SRCS})))
+
+LIB_ARG = $(foreach path, $(LIBS), -L $(dir $(path)) -l $(notdir $(path)))
+
+INCLUDE_ARG = $(addprefix -I ,${INCLUDE_DIR}) $(addprefix -I, $(dir ${LIBS}))
+
+OBJS := $(SRCS:%.c=${OBJS_DIR}/%.o)
+
+IDEPS := $(SRCS:%.c=${OBJS_DIR}/%.d)
+
+CFLAGS := -Wall -Werror -Wextra
+
+all: $(NAME)
+
+$(NAME): ${OBJS_DIR} $(OBJS) $(MAKE_DEP)
+	$(CC) $(CFLAGS) -o $@ $(OBJS) $(LIB_ARG)
+
+${OBJS_DIR}: ${OBJS_DIR_TREE}
+	[ -f .gitignore ] && (grep '^'"$@"'$$' .gitignore || echo "$@" >> .gitignore) || true
+
+${OBJS_DIR_TREE}:
+	mkdir -p $@
+
+${OBJS_DIR}/%.o: ${SRCS_DIR}/%.c
+	$(CC) $(CFLAGS) ${INCLUDE_ARG} -MMD -c $< -o $@
+
+$(MAKE_DEP):
+	make -C $(dir $@) $(notdir $@)
+
+clean:
+	rm -rf $(OBJS_DIR)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: clean all fclean re
+
+.PRECIOUS: ${OBJS_DIR}/%.d
+
+-include ${IDEPS}
